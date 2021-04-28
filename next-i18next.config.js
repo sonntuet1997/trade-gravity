15bc68a113f6ca44b6a1a56e136de9f0167a13a4
sonntuet1997@gmail.com <sonntuet1997@gmail.com>
2021-04-28 15:18:04 +0700

    init

---
+++ next-i18next.config.js
@@ -0,0 +1,13 @@
+// const { nextI18NextRewrites } = require('next-i18next/rewrites')
+
+const localeSubpaths = {
+  en: 'en',
+  ja: 'ja'
+}
+
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja']
+  }
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+const { i18n } = require('./next-i18next.config')
+const path = require('path')
+
+module.exports = {
+  i18n,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  target: 'serverless'
+}
