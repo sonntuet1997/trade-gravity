37991bc0a8aaca6a0a3ec362c4f9b04f5602024b
sonntuet1997@gmail.com <sonntuet1997@gmail.com>
2021-05-04 22:20:49 +0700

    tesst

--- src/checkcoin/index.tsx
+++ src/checkcoin/index.tsx
@@ -5,6 +5,31 @@ import React, {useEffect, useState} from "react";
 import _ from "lodash";
 import Link from 'next/link'
 
+const solve = (a,b,c) => {
+    if(a == 0) {
+        if(b == 0) {
+            if (c == 0) {
+                return [];
+            } else {
+                return [];
+            }
+        } else {
+            return [-c/b];
+        }
+    } else {
+        const delta = b*b - 4*a*c;
+        if(delta > 0) {
+            const x1 = (-b+Math.sqrt(delta))/(2*a);
+            const x2 = (-b-Math.sqrt(delta))/(2*a);
+            return [x1,x2]
+        } else if ( delta == 0) {
+            const sum = -b/2*a
+            return [sum];
+        } else {
+            return [null];
+        }
+    }
+}
 const getBestTrade = (graph, start, coin, gas) => {
     const fee = ( graph['uatom'][finish].rate) * parseFloat(gas);
     if (!graph[start]) return {};
@@ -17,10 +42,10 @@ const getBestTrade = (graph, start, coin, gas) => {
         const b0 = oriV[cur].second;
         const b1 = graph[cur][finish].first;
         const c = graph[cur][finish].second;
-        const x = b0 * c * a1 / (a0+b1+d + b0*b1*d + c * a1);
+        const x = 2 * a0
         const tradeCoin = parseFloat(coin) > x ? x : parseFloat(coin);
-        const conversionRate = ((b0 - tradeCoin)/(a0 + tradeCoin)) * (graph[cur][finish].rate);
-        const originValue = parseFloat(coin)*oriV[finish].rate;
+        const conversionRate = ((b0 - tradeCoin)/(a0 + tradeCoin)) * (c/b1);
+        const originValue = tradeCoin*oriV[finish].rate;
         const profit = conversionRate * tradeCoin - originValue - fee;
         const result = {
             name: cur,
@@ -31,7 +56,10 @@ const getBestTrade = (graph, start, coin, gas) => {
             profitRation: profit/originValue,
             ex: oriV[cur].rate
         }
+        console.log(result);
+        console.log(result.profit)
         if(result.profit > pre.profit) return result;
+        return pre;
     }, {x: 0, profit: 0})
 
     return best
@@ -42,25 +70,27 @@ const finish = 'xrun'
 export const convertedObj = (array) => { 
     return array?.reduce((pre, cur) => {
         const [first,second] = cur;
+        const firstAmount = parseFloat(first.amount);
+        const secondAmount = parseFloat(second.amount);
 
         if(!pre[first.denom]){
             pre[first.denom] = {
-                [second.denom] : { rate :second.amount / first.amount, first: first.amount, second : second.amount}
+                [second.denom] : { rate : secondAmount/ firstAmount, first: firstAmount, second : secondAmount}
             }
         } else {
             pre[first.denom] = {
                 ...pre[first.denom],
-                [second.denom] : { rate :second.amount / first.amount, first: first.amount, second : second.amount}
+                [second.denom] : { rate :secondAmount / firstAmount, first: firstAmount, second : secondAmount}
             }
         }
         if (!pre[second.denom]) {
             pre[second.denom] = {
-                [first.denom] : { rate :first.amount/second.amount, first: second.amount, second : first.amount}
+                [first.denom] : { rate :firstAmount/secondAmount, first: secondAmount, second : firstAmount}
             }
         } else {
             pre[second.denom] = {
                 ...pre[second.denom],
-                [first.denom] : { rate :first.amount/second.amount, first: second.amount, second : first.amount}
+                [first.denom] : { rate :firstAmount/secondAmount, first: secondAmount, second : firstAmount}
             }
         }
         return pre;
@@ -75,7 +105,7 @@ export const CheckCoin = ({data, startPoint} : {data: any, startPoint: string}) 
     const tailLayout = {
         wrapperCol: {offset: 8, span: 8},
     };
-    const [values, setValues] = useState<any>({startPoint, coin:'1000', gas:'0.3'});
+    const [values, setValues] = useState<any>({startPoint, coin:'10000', gas:'0.3'});
     const [result , setResult] = useState<any>({});
     useEffect(()=>{
         if(!(values.startPoint && values.coin && values.gas) ) return;
